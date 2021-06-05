a2ad56f19f960eb3b2fa80b4c97da7b422ecdc17
Thanh Huynh <huynhbathanh@gmail.com>
2021-06-05 15:51:28 +0700

    Add github action

--- src/init.js
+++ src/init.js
@@ -1,10 +1,11 @@
-import gameState from "./gameState";
+import gameState, { handleUserAction } from "./gameState";
 import { TICK_RATE } from "./constants";
 import initButtons from "./buttons";
 
 async function init() {
   console.log("Starting game");
-  initButtons(gameState.handleUserAction);
+  // initButtons(gameState.handleUserAction);
+  initButtons(handleUserAction);
   let nextTimeToTick = Date.now();
 
   function nextAnimationFrame() {
