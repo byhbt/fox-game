801762d3509823cbcb789c5d1f999746b5f8e07e
Thanh Huynh <huynhbathanh@gmail.com>
2021-06-05 21:58:24 +0700

    Update time frame

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ We use [Parcel](https://parceljs.org) bundler to compile the source code in `src
 npm run dev
 ```
 
+# Related topics:
+
+- JS 
+  - [Binding](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind)
+  - [AnimationFrame](https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame)
+- JS Tools
+  - Eslint
+  - Prettier
+  - Parcel Bundler
+- CI
+  - Deploy to Github Pages
+- Programming
+  - Finite State Machine
--- src/gameState.js
+++ src/gameState.js
@@ -1,19 +1,40 @@
-import { modFox, modSence } from "./ui";
-import { RAIN_CHANCE, SCENES, DAY_LENGTH, NIGHT_LENGTH } from "./constants";
+import { modFox, modSence, togglePoopBag } from "./ui";
+import {
+  RAIN_CHANCE,
+  SCENES,
+  DAY_LENGTH,
+  NIGHT_LENGTH,
+  getNextDieTime,
+  getNextHungryTime,
+  getNextPoopTime,
+} from "./constants";
 
 const gameState = {
   current: "INIT",
   clock: 1,
   wakeTime: -1,
   sleepTime: -1,
+  hungryTime: -1,
+  poopTime: -1,
+  dieTime: -1,
+  timeToStartCelebrating: -1,
+  timeToEndCelebrating: -1,
   tick() {
     this.clock++;
-    console.log("clock", this.clock);
+    console.log("clock Count: ", this.clock);
 
     if (this.clock === this.wakeTime) {
       this.wake();
     } else if (this.clock === this.sleepTime) {
       this.sleep();
+    } else if (this.clock === this.hungryTime) {
+      this.getHungry();
+    } else if (this.clock === this.timeToStartCelebrating) {
+      this.startCelebrating();
+    } else if (this.clock === this.timeToEndCelebrating) {
+      this.endCelebrating();
+    } else if (this.clock === this.dieTime) {
+      this.die();
     }
 
     return this.clock;
@@ -29,16 +50,28 @@ const gameState = {
     this.current = "IDLING";
     this.wakeTime = -1;
 
-    modFox("idling");
+    // modFox("idling");
     this.scence = Math.random() > RAIN_CHANCE ? 0 : 1;
     modSence(SCENES[this.scene]);
     this.sleepTime = this.clock + DAY_LENGTH;
+    this.hungryTime = getNextHungryTime(this.clock);
+    this.determineFoxState();
   },
   sleep() {
     this.state = "SLEEP";
     modFox("sleep");
     modSence("night");
     this.wakeTime = this.clock + NIGHT_LENGTH;
+  },
+  getHungry() {
+    console.log("hungry");
+    this.current = "HUNGRY";
+    this.dieTime = getNextDieTime(this.clock);
+    this.hungryTime = -1;
+    modFox("hungry");
+  },
+  die() {
+    console.log("die");
   },
   handleUserAction(icon) {
     if (
@@ -59,7 +92,7 @@ const gameState = {
       case "poop":
         this.cleanUpPoop();
         break;
-      case "Feed":
+      case "fish":
         this.feed();
         break;
     }
@@ -68,10 +101,55 @@ const gameState = {
     console.log("Change weather");
   },
   cleanUpPoop() {
-    console.log("Poop");
+    if (this.current === "POOPING") {
+      return;
+    }
+
+    this.dieTime = -1;
+    togglePoopBag(true);
+    this.startCelebrating();
+  },
+  startCelebrating() {
+    this.current = "CELEBRATING";
+    modFox("celebrate");
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = this.clock + 2;
+  },
+  endCelebrating() {
+    this.timeToEndCelebrating = -1;
+    this.current = "IDLING";
+    this.determineFoxState();
+
+    togglePoopBag(false);
+  },
+  determineFoxState() {
+    if (this.current == "IDLING") {
+      if (SCENES[this.scence] == "rain") {
+        modFox("rain");
+      } else {
+        modFox("idling");
+      }
+    }
   },
   feed() {
     console.log("Feed");
+
+    if (this.current !== "HUNGRY") {
+      return;
+    }
+
+    this.current = "FEEDING";
+    this.dieTime = -1;
+    this.poopTime = getNextPoopTime(this.clock);
+    modFox("eating");
+
+    this.timeToStartCelebrating = this.clock + 2;
+  },
+  poop() {
+    this.current = "POOPING";
+    this.poopTime = -1;
+    this.dieTime = getNextDieTime(this.clock);
+    modFox("pooping");
   },
 };
 
